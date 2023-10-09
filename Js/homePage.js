66bae680224709bee7f04684fe56f4002663f47a
Janith Dilshan <janithdil78@gmail.com>
2023-10-09 23:28:25 +0530

    done

--- Js/homePage.js
+++ Js/homePage.js
@@ -1,7 +1,19 @@
 let menu = document.querySelector("#menu-icon");
-let navBar = document.querySelector(".nav-bar");
+let navbar = document.querySelector(".nav-bar");
 
 menu.onclick = () => {
-  menu.classList.toggle("bx-x");
-  navBar.classList.toggle("");
+  navbar.classList.toggle("open");
 };
+const sr = ScrollReveal({
+  distance: "40px",
+  duration: 2500,
+  reset: true,
+});
+sr.reveal(".logo", { delay: 300, origin: "left" });
+sr.reveal(".nav-bar", { delay: 600, origin: "top" });
+
+sr.reveal(".home-text span", { delay: 400, origin: "top" });
+sr.reveal(".home-text h1", { delay: 600, origin: "left" });
+sr.reveal(".home-text p", { delay: 720, origin: "right" });
+
+sr.reveal(".main-btns", { delay: 870, origin: "bottom" });
